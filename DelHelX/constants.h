9f66e0a6aa1201275d73d7bfc791387754763488
sushi.at <sushi@opensky.to>
2025-04-01 10:35:08 +0100

    Bumped version number to 0.2.0

--- DelHelX/constants.h
+++ DelHelX/constants.h
@@ -3,7 +3,7 @@
 #include <regex>
 
 #define PLUGIN_NAME    "DelHelX"
-#define PLUGIN_VERSION "0.1.0"
+#define PLUGIN_VERSION "0.2.0"
 #define PLUGIN_AUTHOR  "Markus Korbel"
 #define PLUGIN_LICENSE "(c) 2025, MIT License"
 #define PLUGIN_LATEST_VERSION_URL "https://raw.githubusercontent.com/sushiat/DelHelX/master/version.txt"
